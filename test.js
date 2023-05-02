3f90bce3d8dafbf5e18c96b6d40668877ac8181a
BunkerStriker <130099468+BunkerStriker@users.noreply.github.com>
2023-05-02 10:47:08 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,195 +1,350 @@
 (function (Scratch) {
-    // ============== linear algebra ============== 
+  'use strict';
+  console.log("ShovelUtils v1.3");
+  const vm = Scratch.vm;
 
-    const from_s = s => (''+s).split(' ').map(s=> s.split(',').map(v=> isNaN(+v) ? 0 : +v))
-    const to_s = m => m.map(v=> v.join(',')).join(' ')
-
-    const safe_index = (m,i) => m[m.length == 1 ? 0 : i]
-
-    const component_wise = f => (a,b) => {
-        let [m,n] = a.length > b.length ? [a,b] : [b,a]
-        return m.map((v,i)=>  safe_index(n,i) == undefined ? v : f(safe_index(a,i), safe_index(b,i)))
+  // Based on from https://www.growingwiththeweb.com/2017/12/fast-simple-js-fps-counter.html
+  const times = [];
+  let fps = vm.runtime.frameLoop.framerate;
+  const oldStep = vm.runtime._step;
+  vm.runtime._step = function () {
+    oldStep.call(this);
+    const now = performance.now();
+    while (times.length > 0 && times[0] <= now - 1000) {
+      times.shift();
     }
-    const component_wise2D = f => (a,b) => component_wise(component_wise(f))(a,b)
-    const add = component_wise((a,b)=>a+b)
-    const mul = component_wise((a,b)=>a*b)
-    const add2D = component_wise2D((a,b)=>a+b)
-    const sub2D = component_wise2D((a,b)=>a-b)
-    const mul2D = component_wise2D((a,b)=>a*b)
-    const div2D = component_wise2D((a,b)=>a/b)
+    times.push(now);
+    fps = times.length;
+  };
 
-    const set = (i,m,v) => {
-        if(m.length == 1) {
-            if(m[0][i-1] != undefined)
-                m[0][i-1] = v[0][0]
-        }
-        else {
-            m[i-1] = v[0]
-        }
-        return m
-    }
-    const get = (i,m) => [ m.length == 1 ? ( [m[0][i-1]] ?? [] ) : ( m[i-1] ?? [] ) ]
+  class ShovelUtils {
+    getInfo() {
+      return {
+        id: 'ShovelUtils',
+        name: 'ShovelUtils',
+        color1: '#f54242',
+        color2: '#f54242',
+        color3: '#f54242',
+        blocks: [
+          {
+            opcode: 'importImage',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Import image from [TEXT] name [NAME]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://extensions.turbowarp.org/dango.png',
+              },
+              NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Apple',
+              }
+            }
+          },
+          {
+            opcode: 'getlist',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Get list [TEXT]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'MyList',
+              }
+            }
+          },
+          {
+            opcode: 'setlist',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Set list [NAME] to [TEXT]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '[1,2]',
+              },
+              NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'MyList',
+              }
+            }
+          },
+          {
+            opcode: 'importSprite',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Import sprite from [TEXT]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Link or data uri here',
+              }
+            }
+          },
+          {
+            opcode: 'importSound',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Import sound from [TEXT] name [NAME]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://theshovel.github.io/kewlab-data/char%20edit.mp3',
+              },
+              NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Apple',
+              }
+            }
+          },
+          {
+            opcode: 'importProject',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Import project from [TEXT]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://theshovel.github.io/Bullet-Hell/Bullet%20Hell',
+              }
+            }
+          },
+          {
+            opcode: 'loadExtension',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Load extension from [TEXT]',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://extensions.turbowarp.org/utilities.js',
+              }
+            }
+          },
 
-    const dot = (a,b) => {
-        if(a.length == 1 && a[0].length == 3)
-            a = a[0].map(v=>[v])
-        return b.map(bv=> a.reduce((acc,av,i)=> add(acc,mul(av,[safe_index(bv,i)])), 0))
-    }
+          {
+            opcode: 'restartProject',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Restart project',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '0',
+              }
+            }
+          },
+          {
+            opcode: 'evalBlock',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Javascript',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'javascript code',
+              }
+            }
+          },
+          {
+            opcode: 'deleteSprite',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Delete sprite [SPRITE]',
+            arguments: {
+              SPRITE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Sprite1',
+              }
+            }
+          },
+          {
+            opcode: 'setedtarget',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Set editing target to [NAME]',
+            arguments: {
+              NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Sprite1',
+              }
+            }
+          },
 
-    const det = (m,i1,i2) => m[0][i1%3]*m[1][i2%3]-m[0][i2%3]*m[1][i1%3]
-    const cross = component_wise((a,b)=> a.map((_,i)=>det([a,b], i+1, i+2)))
+          {
+            opcode: 'brightnessByColor',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Get brightness of [color]',
+            arguments: {
+              color: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '#ffffff',
+              }
+            }
+          },
 
-    const length = m => m.map(v=> dot([v],[v]).map(v=> Math.sqrt(v)))
-    const normalize = m => div2D(m,length(m))
-
-    const rotate = (a,v) => {
-        a = a[0][0] * Math.PI / 180
-        v = normalize(v)[0]
-
-        const s = Math.sin(a)
-        const c = Math.cos(a)
-
-        const f1 = i => c + v[i] * v[i] * (1-c)
-        const f2 = (i, n) => v[(i+1)%3] * v[(i+2)%3] * (1-c) + n * v[i] * s
-
-        return [
-            [f1(0), f2(2,1), f2(1,-1)],
-            [f2(2,-1), f1(1), f2(0,1)],
-            [f2(1,1), f2(0,-1), f1(2)]
+          {
+            opcode: 'getfps',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'Fps'
+          },
         ]
+      };
     }
 
-    // =================== Scratch extension =================== 
-
-    // auto arguments is a little over complicated to deduce argument count
-
-    const letter = i => String.fromCharCode(97+i)
-
-    const auto_block = (blockType, opcode, text, args) => ({
-        blockType,
-        opcode,
-        text,
-        arguments: Object.fromEntries(
-            new Array(text.split('[').length-1).fill().map((_,i)=> [
-                letter(i), {
-                    type: (args && args[i]) || "number", 
-                    defaultValue: " "
-                }
-            ])
-        )
-    })
-    const mat_reporter_f = f => o => to_s(f(...new Array(Object.entries(o).length).fill().map((_,i)=> from_s(o[letter(i)]))))
-
-    class ScratchMath {
-
-        constructor(runtime) {
-            this.runtime = runtime
-        }
-
-        getInfo() {
-            return {
-                id: "math",
-                name: "Math",
-                blocks: [
-                auto_block('reporter', "Vec", "vector [a] [b] [c]"),
-                auto_block('reporter', "Arr", "list [a] [b]"),
-                auto_block('reporter', "Get", "item [a] of [b]"),
-                auto_block('reporter', "Set", "with item [a] of [b] = [c]"),
-                auto_block('reporter', "Rot", "rotate [a] around [b]", ["angle"]),
-                {
-                    blockType: 'command',
-                    opcode: 'Out',
-                    text: 'output [a] to [b]',
-                    arguments: {
-                        a: {
-                            type: "number",
-                            defaultValue: " "
-                        },
-                        b: {
-                            type: "string",
-                            defaultValue:" ",
-                            menu: "varMenu"
-                        }
-                    }
-                },
-
-                '---',
-
-                auto_block('reporter', "Add", '[a] + [b]'),
-                auto_block('reporter', "Sub", '[a] - [b]'),
-                auto_block('reporter', "Mul", '[a] * [b]'),
-                auto_block('reporter', "Div", '[a] / [b]'),
-                auto_block('reporter', "Dot", '[a] dot [b]'),
-                auto_block('reporter', "Cross", '[a] cross [b]'),
-                auto_block('reporter', "Len", 'length of [a]'),
-                auto_block('reporter', "Norm",'normalize [a]'),
-                auto_block('reporter', "Size",'size of [a]'),
-                auto_block('reporter', "Sqrt",'sqrt of [a]'),
-                
-                ],
-                menus: {
-                    varMenu: 'getVarMenu'
-                }
-            }
-        }
-
-        getVarMenu(target_id) {
-            const vars = this.runtime.getTargetById(target_id).getAllVariableNamesInScopeByType('list')
-            return vars.length == 0 ? [" "] : vars
-        }
-
-        Vec({a,b,c}) {
-            return to_s([[a,b,c]])
-        }
-
-        Out({a,b}, util) {
-            let variable = util.target.lookupOrCreateList(undefined, b);
-            if(variable)
-                variable.value = a.split(' ');
-        }
-
-        Get   = mat_reporter_f(get)
-        Set   = mat_reporter_f(set)
-        Arr   = mat_reporter_f((a,b) => [...a, ...b])
-        Rot   = mat_reporter_f(rotate)
-        Add   = mat_reporter_f(add2D)
-        Sub   = mat_reporter_f(sub2D)
-        Mul   = mat_reporter_f(mul2D)
-        Div   = mat_reporter_f(div2D)
-        Dot   = mat_reporter_f(dot)
-        Cross = mat_reporter_f(cross)
-        Len   = mat_reporter_f(length)
-        Norm  = mat_reporter_f(normalize)
-        Size  = mat_reporter_f(m => [[m.length]])
-        Sqrt  = mat_reporter_f(a=> component_wise2D((a,b)=>Math.sqrt(a))(a,[[1]]))
+    importImage({ TEXT, NAME }) {
+      Scratch.fetch(TEXT)
+        .then((r) => r.arrayBuffer())
+        .then((arrayBuffer) => {
+          const storage = vm.runtime.storage;
+          vm.addCostume(NAME + '.PNG', {
+            name: NAME,
+            asset: new storage.Asset(
+              storage.AssetType.ImageBitmap,
+              null, // asset id, doesn't need to be set here because of `true` at the end will make Scratch generate it for you
+              storage.DataFormat.PNG,
+              new Uint8Array(arrayBuffer),
+              true
+            )
+          });
+        });
     }
 
-    // ============== globalize vm and load extension ===============
-
-    function findReactComponent(element) {
-        let fiber = element[Object.keys(element).find(key => key.startsWith("__reactInternalInstance$"))];
-        if (fiber == null) return null;
-
-        const go = fiber => {
-            let parent = fiber.return;
-            while (typeof parent.type == "string") {
-                parent = parent.return;
-            }
-            return parent;
-        };
-        fiber = go(fiber);
-        while(fiber.stateNode == null) {
-            fiber = go(fiber);
-        }
-        return fiber.stateNode;
+    importSprite({ TEXT }) {
+      Scratch.fetch(TEXT)
+        .then(r => r.arrayBuffer())
+        .then(buffer => vm.addSprite(buffer))
+        .then(() => {
+          console.log("Done");
+        })
+        .catch((error) => {
+          console.log("Error", error);
+        });
     }
 
-    window.vm = findReactComponent(document.getElementsByClassName("stage-header_stage-size-row_14N65")[0]).props.vm;
-  Scratch.extensions.register(new ScratchMath());
+    deleteSprite({ SPRITE }) {
+      const target = vm.runtime.getSpriteTargetByName(SPRITE);
+      if (!target || target.isStage) {
+        return;
+      }
+      // @ts-expect-error
+      if (typeof ScratchBlocks !== 'undefined') {
+        if (!confirm(`Do you want to delete the sprite "${SPRITE}"? This cannot be undone.`)) {
+          return;
+        }
+      }
+      vm.deleteSprite(target.id);
+    }
+
+    importSound({ TEXT, NAME }) {
+      Scratch.fetch(TEXT)
+        .then((r) => r.arrayBuffer())
+        .then((arrayBuffer) => {
+          const storage = vm.runtime.storage;
+          const asset = new storage.Asset(
+            storage.AssetType.Sound,
+            null,
+            storage.DataFormat.MP3,
+            new Uint8Array(arrayBuffer),
+            true
+          );
+          vm.addSound({
+            md5: asset.assetId + '.' + asset.dataFormat,
+            asset: asset,
+            name: NAME
+          });
+        });
+    }
+
+    importProject({ TEXT }) {
+      // @ts-ignore
+      if (typeof ScratchBlocks !== 'undefined') {
+        // We are in the editor. Ask before loading a new project to avoid unrecoverable data loss.
+        if (!confirm(`Do you want to import a project from "${TEXT}"? Everything in the current project will be permanently deleted.`)) {
+          return;
+        }
+      }
+      Scratch.fetch(TEXT)
+        .then(r => r.arrayBuffer())
+        .then(buffer => vm.loadProject(buffer))
+        .then(() => {
+          console.log("Done");
+          vm.greenFlag();
+        })
+        .catch((error) => {
+          console.log("Error", error);
+        });
+    }
+
+    restartProject() {
+      vm.greenFlag();
+    }
+
+    evalBlock({ TEXT }) {
+      eval(TEXT);
+    }
+
+    async loadExtension({ TEXT }) {
+      if (await vm.securityManager.canLoadExtensionFromProject(TEXT)) {
+        vm.extensionManager.loadExtensionURL(TEXT);
+      }
+    }
+
+    getlist({ TEXT }) {
+      const list = vm.runtime.getTargetForStage().lookupVariableByNameAndType(TEXT, 'list');
+      if (list) {
+        return JSON.stringify(list.value);
+      } else {
+        return "";
+      }
+    }
+    setlist({ TEXT, NAME }) {
+      let parsed;
+      try {
+        parsed = JSON.parse(TEXT);
+      } catch (e) {
+        return; // JSON was invalid
+      }
+
+      if (!Array.isArray(parsed)) {
+        return; // it's not an array
+      }
+
+      for (const element of parsed) {
+        const type = typeof element;
+        if (type !== "string" && type !== "number" && type !== "boolean") {
+          return; // One of the elements has a disallowed type
+        }
+      }
+
+      const list = vm.runtime.getTargetForStage().lookupVariableByNameAndType(NAME, 'list');
+      if (!list) {
+        return; // List was not found
+      }
+
+      list.value = parsed;
+    }
+
+    setedtarget({ NAME }) {
+      let target;
+
+      //I know this might cause sprites called "stage" to be ignored. But lets be real, who names their sprite "stage"?
+      if (NAME.toLowerCase() === "stage") {
+        target = vm.runtime.getTargetForStage();
+      } else {
+        target = vm.runtime.getSpriteTargetByName(NAME);
+      }
+      if (target) {
+        vm.setEditingTarget(target.id);
+      }
+    }
+
+    /**
+     * Calculate brightness value by RGB or HEX color.
+     * @param color (String) The color value in RGB or HEX (for example: #000000 || #000 || rgb(0,0,0) || rgba(0,0,0,0))
+     * @returns (Number) The brightness value (dark) 0 ... 255 (light)
+     */
+    brightnessByColor({ color }) {
+      // https://www.w3.org/TR/AERT/#color-contrast
+      const {r, g, b} = Scratch.Cast.toRgbColorObject(color);
+      return ((r * 299) + (g * 587) + (b * 114)) / 1000;
+    }
+
+    getfps(){
+      return fps;
+    }
+  }
+
+  Scratch.extensions.register(new ShovelUtils());
+// @ts-ignore
 })(Scratch);
-
-(function() {
-    var extensionInstance = new ScratchMath(window.vm.extensionManager.runtime)
-    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
-    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
-})()
